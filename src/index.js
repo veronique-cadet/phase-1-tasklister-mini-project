e81b237006b2ccd1a25ea89cd5de634bfc33ed26
Veronique Cadet <cadetvero@gmail.com>
2022-11-26 18:51:40 -0500

    final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputField = document.getElementById("new-task-description");
+    createNewTask(inputField.value);
+    form.reset();
+  });
+  function createNewTask(task) {
+    const ul = document.querySelector("#tasks");
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    const editBtn = document.createElement("button");
+    btn.addEventListener("click", () => {
+      li.remove();
+    });
+    editBtn.addEventListener("click", () => {
+      li.contentEditable = true;
+      btn.contentEditable = false;
+    });
+    btn.innerText = " x ";
+    editBtn.innerText = "edit";
+    li.innerText = `${task}  `;
+    li.appendChild(btn);
+    li.appendChild(editBtn);
+    ul.append(li);
+  }
 });
